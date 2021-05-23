1d22931f4cd4cd1027fce47c59ea10df9f33031b
t0mi91 <79869222+t0mi91@users.noreply.github.com>
2021-05-23 21:27:32 +0200

    practice

--- 11_Javascript/BuildFacebook2/script.js
+++ 11_Javascript/BuildFacebook2/script.js
@@ -1,50 +1,57 @@
-var dataBase = [
-{  
-    username: "t0mi91",
-    password: "12345"
-},
-{   
-    username: "sally",
-    password: "123"
-},
-{
-    username: "ingrid",
-    password: "777"
-}
+var database =[
+    {
+        username: "Tomi",
+        password: "12345"
+    },
+    {
+        username: "Vixi",
+        password: "54321"
+    },
+    {
+        username: "Ingrid",
+        password: "777"
+    }
 ];
 
-var newsFeed = [
-    {username: "Vixi",
-    timeline: "What a rainy day!"},
-
-    {username: "Zsolti",
-    timeline: "Yes, it is a rainy day!"},
-
-    {username: "Szandi",
-    timeline: "The weather is cold too."}
+var newsfeed = [
+    {
+        username: "Marcus",
+        timeline: "I am a Quanon freak.",
+    },
+    {
+        username: "Remus",
+        timeline: "I am a werewolf.",
+    },
+    {
+        username: "Tonks",
+        timeline: "I can transform my hair."
+    },
+    {
+        username: "Bruce",
+        timeline: "I took over for God for the week."
+    }
 ];
 
-var userNamePrompt = prompt("What is your username?");
-var passwordPrompt = prompt("What is your password?");
+var userNamePrompt = prompt("What\'s your username?");
+var passwordPrompt = prompt("What\'s your password?");
 
-
-function isUserValid (username, password){
-    for (var i=0; i<dataBase.length; i++){
-        if(dataBase[i].username ===username &&
-            dataBase[i].password === password){
-                return true;
-            }
+function validateUser(username, password){
+    for(i=0; i<database.length; i++){
+        if (database[i].username ===username &&
+            database[i].password ===password){
+            return true;
+        }   
     }
     return false;
 }
 
-function SignIn(username, password){
-    if(isUserValid(username, password)){
-        console.log(newsFeed);
+function signIn (username, password){
+    if (validateUser(username, password)){
+        console.log(newsfeed);
     }
-    else{
-        alert("Sorry, wrong username or password.");
+    else {
+        alert("Sorry, wrong username or password. Cannot log you in.");
     }
 }
 
-SignIn(userNamePrompt, passwordPrompt);
+signIn(userNamePrompt, passwordPrompt);
